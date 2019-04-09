f37aee97d2e8b7bb5f91b2c91479329b6a0ce426
dai <msdenisseanaya@gmail.com>
2019-04-09 15:27:44 -0700

    first commit -character and background are available

---
+++ android/src/com/coingrabngo/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.coingrabngo.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.coingrabngo.game.CoinGrabNGo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinGrabNGo(), config);
+	}
+}
---
+++ core/src/com/coingrabngo/game/CoinGrabNGo.java
@@ -0,0 +1,40 @@
+package com.coingrabngo.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import javax.xml.soap.Text;
+
+public class CoinGrabNGo extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+	Texture[] character;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("bg.png");
+		character = new Texture[4];
+		character[0] = new Texture("frame-1.png");
+		character[1] = new Texture("frame-2.png");
+		character[2] = new Texture("frame-3.png");
+		character[3] = new Texture("frame-4.png");
+	}
+
+	@Override
+	public void render () {
+		batch.begin();
+		batch.draw(background,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(character[0],Gdx.graphics.getWidth()/2- character[0].getWidth()/2, Gdx.graphics.getHeight()/2);
+		batch.end();
+
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
